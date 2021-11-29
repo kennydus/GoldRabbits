3ef41051b4a507156df51e3b50485ebf97287d26
Kenny Du <kennydus@gmail.com>
2021-11-28 20:29:45 -0800

    Transferred GoldRabbits functions into "GoldRabbits.tools"

--- main.cpp
+++ main.cpp
@@ -1,64 +1,23 @@
-#include <iostream>
-#include<time.h>
-#include<iomanip>
-#include <string>
+//  Kenny Du
+//  Class (CECS 282-01)
+//  Project Name (Prog 5 – STL (GoldRabbits))
+//  Due Date (11/29/21)
+//
+//  I certify that this program is my own original work. I did not copy any part of this program from
+//  any other source. I further certify that I typed each and every line of code in this program.
+
 #include <climits>
+#include <iostream>
+#include <string>
 #include <map>
 
 #include "BigInt.h"
 
 using namespace std;
 
-int goldRabbits(int); // prototype or signature
+#include "GoldRabbits.tools"
 
 void pause() {cout << "Press Enter to continue..."; getchar();} // utility function
-
-
-
-
-BigInt goldRabbits(BigInt n) {
-    static map<BigInt, BigInt> rabbit_map;
-
-    // insert value BigInt(1) if n is 0 or 1
-    if (n <= BigInt(1)) {
-        rabbit_map.insert({n, BigInt(1)});
-    }
-
-    // check if the key exists for 'n'
-    else if (rabbit_map.find(n) == rabbit_map.end())
-        //it does not exist
-        // make a key in the map and return the value of the key
-        rabbit_map[n] = goldRabbits(n - BigInt(1)) + goldRabbits(n - BigInt(2));
-
-    // value now exists for key 'n' inside of map- return the value
-    return rabbit_map.at(n);
-}
-
-// this is the goldRabbits function
-int goldRabbits(int n)
-{
-    // keeps track of all previously calculated results for goldRabbits(n)
-    static map<int, int> int_rabbit_map;
-
-    if (n==0 || n==1)
-        int_rabbit_map.insert({n, 1});
-
-    // if the key doesn't exist
-    else if (int_rabbit_map.find(n) == int_rabbit_map.end()){
-        int rabbits = goldRabbits(n - 1) + goldRabbits(n - 2);
-        // checks for overflow. if rabbits < 0 (or negative), then we have overflow
-        if (rabbits > 0)
-            int_rabbit_map.insert({n, rabbits});
-        else{
-            string except = "Overflow Error - goldRabbits overflowed using " + to_string(n);
-            throw except;
-        }
-    }
-        return int_rabbit_map.at(n);
-}
-
-
-
 
 int main()
 {
@@ -94,39 +53,10 @@ int main()
     }
     pause();
 
-//    B1++;
-//    cout<<"\nB1++:";
-//    B1.print();
-//    BigInt b10 = B1 + BigInt(1);
-//    cout << "\nb10: ";
-//    b10.print();
-//
-//    BigInt b11(0);
-//    cout << "\nb11: ";
-//    b11.print();
-//    b11 = b11 + BigInt(1);
-//    cout << "b11 after + BigInt(1): ";
-//    b11.print();
-//
-//    BigInt b12(3000);
-//    b12 = b12 - BigInt(2);
-//    cout << "\nb12:" << b12 << endl;
-//    b12 = B1;
-//    cout << "\nb12:";
-//    b12.print();
-//    B1++;
-//    cout << "\nb12:";
-//    b12.print();
-//    BigInt b30 = MAX;
-//    cout << "\nb30:" << b30 << endl;
-//    MAX = MAX - BigInt(10);
-//    cout << "\nb30:" << b30 << endl;
-//
-//    cout<< "BigInt(50):" << BigInt(50) << endl;
-//
-//    cout << "\ncomparing 51 and 50: " << (BigInt(51) <= BigInt(50)) << endl;
-
+    cout << goldRabbits(BigInt(500));
 }
+
+
 
 
 
